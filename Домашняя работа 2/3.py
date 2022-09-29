e158a173ad96508af2189e0e06e68a04c8b21886
burmistrov1337 <burmistrov.ma@mail.ru>
2022-09-29 14:15:34 +0700

    fix

---
+++ Домашняя работа 2/3.py
@@ -0,0 +1,12 @@
+# 3) Задайте список (словарь не нужно выводить!) из n чисел последовательности (1+1/n)^n и выведите на экран их сумму.
+
+# n = int(input("Введите число N: "))
+
+n = int(input('Enter number: ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print(round(sum(sequence(n))))

a86bdc77fdb0170c5c43fc1c1680693e4039fc34
burmistrov1337 <burmistrov.ma@mail.ru>
2022-10-06 16:41:03 +0700

    dz3

--- Домашняя работа 3/3.py
+++ Домашняя работа 3/3.py
@@ -4,24 +4,35 @@
 
 # - [1.1, 1.2, 3.1, 10.01] => 0.19
 
-numbers = [1.1, 1.2, 3.1, 10.01]
+numbers = [1.1, 1.2, 3.1, 4.3, 10.02]
+
 
 def pairs(numbers):
     results = []
-    while len(numbers) > 1:
-        results.append(numbers[0]-int(numbers[-1]))
+    while len(numbers) != 0:
+        st = round((numbers[0]-int(numbers[0])), 2)
+        results.append(st)
         del numbers[0]
-        del numbers[-1]
     return results
-print(pairs(numbers))
 
-# n = array
-# max = 0
+
+array = pairs(numbers)
+array1 = max(array) - min(array)
+print(f'{array1:.2f}')
+
+
+# array = pairs(numbers)
+
+# max = [0]
+# max1 = [i for i in array if array[i] > max]
+# print(max1)
 # min = [0]
-# for i in range(len(n)):
-#     if n[i]>max:
-#         max = n[i]
-#     elif n[i]<min:
-#         min = n[i]
+
+# for i in range(len(array)):
+#     if array[i] > max:
+#         max = array[i]
+#     elif array[i] < min:
+#         min = array[i]
+
 # result = max - min
 # print(f"Разница между максимальным и минимальным значением дробной части элементов равна: {result}")
---
+++ Домашняя работа 3/4.py
@@ -0,0 +1,13 @@
+# lst = list(map(float, input("Введите числа через пробел:\n").split()))
+# new_lst = [round(i%1,2) for i in lst if i%1 != 0]
+# print(max(new_lst) - min(new_lst))
+
+my_list = [1.1, 1.2, 3.1, 5, 10.01]
+min = 1
+max = 0
+for i in my_list:
+    if (i - int(i)) <= min:
+        min = i - int(i)
+    if (i - int(i)) >= max:
+        max = i - int(i)  
+print(max-min)

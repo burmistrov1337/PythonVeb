867d1fdecf214410e67fedb049e431786b3433bb
burmistrov1337 <burmistrov.ma@mail.ru>
2022-10-07 20:51:23 +0700

    dz3

--- Домашняя работа 3/4.py
+++ Домашняя работа 3/4.py
@@ -1,13 +1,11 @@
-# lst = list(map(float, input("Введите числа через пробел:\n").split()))
-# new_lst = [round(i%1,2) for i in lst if i%1 != 0]
-# print(max(new_lst) - min(new_lst))
+# Задача №25: Напишите программу, которая будет преобразовывать десятичное число в двоичное. Пример:
+# 45 -> 101101
+# 3 -> 11
+# 2 -> 10
 
-my_list = [1.1, 1.2, 3.1, 5, 10.01]
-min = 1
-max = 0
-for i in my_list:
-    if (i - int(i)) <= min:
-        min = i - int(i)
-    if (i - int(i)) >= max:
-        max = i - int(i)  
-print(max-min)
+n = int(input("Введите число: \n"))
+s = ""
+while n != 0:
+    s = str(n%2) + s
+    n //=2
+print(s)

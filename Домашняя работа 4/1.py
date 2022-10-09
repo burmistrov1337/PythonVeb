8c726bf5a04961040a69af1f68169cd0f760bc72
burmistrov1337 <burmistrov.ma@mail.ru>
2022-10-09 14:07:13 +0700

    veb5

--- Домашняя работа 4/1.py
+++ Домашняя работа 4/1.py
@@ -30,16 +30,9 @@
 
 # print(format(mypi, '.4'))
 
-
-
 k = 1
-s = 0
-
-for i in range(10000000):
-    if i % 2 == 0:
-        s += 4/k
-    else:
-        s -= 4/k
-    k += 2
-
-print(s)
+x = 0
+for k in range(1, 1000000):
+    x = x+4*((-1)**(k+1))/(2*k-1)
+    
+print(x)

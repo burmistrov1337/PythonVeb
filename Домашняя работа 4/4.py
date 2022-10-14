169b9e4f679218c3ac5e197cb5bb76f651ef9abe
burmistrov1337 <burmistrov.ma@mail.ru>
2022-10-14 21:25:09 +0700

    dz4

--- Домашняя работа 4/4.py
+++ Домашняя работа 4/4.py
@@ -6,3 +6,20 @@
 # список: ["123", "234", 123, "567"], ищем: "123", ответ: -1
 # список: [], ищем: "123", ответ: -1
 
+test_list = ["qwe", "asd", "zxc", "qwe", "ertqwe"]
+print(f"список: {test_list}")
+test_item = input("Введите искомую строку: ")
+
+
+def check_list(test_list, test_item):
+    count = 0
+    for i in range(len(test_list)):
+        if test_list[i] == test_item:
+            count += 1
+            if count == 2:
+                return i
+    else:
+        return -1
+
+
+print(f"ответ: {check_list(test_list, test_item)}")

c7fb37bd9f6dbd02f4f5e4d0036a99699ba8d04b
burmistrov1337 <burmistrov.ma@mail.ru>
2022-10-22 16:26:36 +0700

    veb9

---
+++ Вебинар 9/1.py
@@ -0,0 +1,39 @@
+# from progress.bar import Bar
+# import time
+# bar = Bar('Processing', max=20)
+# for i in range(20):
+#     # Do some work
+#     time.sleep(1)
+#     bar.next()
+# bar.finish()
+
+# import emoji
+# print(emoji.emojize('Python is :thumbs_up:'))
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# # Fixing random state for reproducibility
+# np.random.seed(19680801)
+
+
+# plt.rcdefaults()
+# fig, ax = plt.subplots()
+
+# # Example data
+# people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+# y_pos = np.arange(len(people))
+# performance = 3 + 10 * np.random.rand(len(people))
+# error = np.random.rand(len(people))
+
+# ax.barh(y_pos, performance, xerr=error, align='center')
+# ax.set_yticks(y_pos, labels=people)
+# ax.invert_yaxis()  # labels read top-to-bottom
+# ax.set_xlabel('Performance')
+# ax.set_title('How fast do you want to go today?')
+
+# list = [1,2,3,2,7]
+# plt.plot(list, )
+
+# plt.show()
+
